4b4ea8e2b4dd3dd4646cd5e18e993a4bac9ea3b3
black-tiger4 <3030951264@qq.com>
2024-05-21 16:34:13 +0800

    打包

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tetris/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
   ],
